1d62b0f05fe1c79fff6414eaaa4f616ec2a8662a
cschen <gamma_chen@yahoo.com.tw>
2019-12-12 14:51:44 +0800

    refine

--- cmake/1/func1.cpp
+++ cmake/1/func1.cpp
@@ -4,8 +4,8 @@ int func1() {
   std::ifstream is("foobar.txt");     // open file
 
   int i = 0;
-  char c;
-  while (is.get(c)) {        // loop getting single characters
+  char str[1024];
+  while (is.getline(str, 1024)) {        // loop getting single characters
     i++;
   }
 
--- cmake/1/main.cpp
+++ cmake/1/main.cpp
@@ -6,7 +6,7 @@ extern int func2();
 using namespace std;
 
 int main() {
-  cout << "total is = " << func1() << " characters" << endl;
+  cout << "total is = " << func1() << " lines" << endl;
   cout << "file content is : " << endl;
   func2();
   return 0;
