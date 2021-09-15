040e5f8672e8ee59c1998faa61c82da05c788c3d
ChungShu <gamma_chen@yahoo.com.tw>
2021-09-15 14:34:05 +0800

    llvm path

--- riscv/1/ch6_1.cpp
+++ riscv/1/ch6_1.cpp
@@ -1,9 +1,10 @@
-// ~/llvm/release/build/bin/clang -target riscv64-unknown-linux-gnu -S ch6_1.cpp -emit-llvm -o ch6_1.ll
-// ~/llvm/release/build/bin/llc -march=riscv64 -mcpu=generic-rv64 -relocation-model=static -filetype=asm ch6_1.ll -o -
-// ~/llvm/release/build/bin/llc -march=riscv64 -mcpu=generic-rv64 -relocation-model=static -filetype=obj ch6_1.ll -o ch6_1.o
-// ~/llvm/release/build/bin/llvm-readelf -tr ch6_1.o
-// ~/llvm/release/build/bin/lld -flavor gnu ch6_1.o -o a.out
-// ~/llvm/release/build/bin/llvm-objdump -d a.out
+// ~/llvm/13/llvm-project/build/bin/clang -target riscv64-unknown-linux-gnu -S ch6_1.cpp -emit-llvm -o ch6_1.ll
+// ~/llvm/13/llvm-project/build/bin/clang -target riscv64-unknown-linux-gnu -c -mno-relax ch6_1.cpp -o ch6_1.o
+// ~/llvm/13/llvm-project/build/bin/llc -march=riscv64 -mcpu=generic-rv64 -relocation-model=static -filetype=asm ch6_1.ll -o -
+// ~/llvm/13/llvm-project/build/bin/llc -march=riscv64 -mcpu=generic-rv64 -relocation-model=static -filetype=obj ch6_1.ll -o ch6_1.o
+// ~/llvm/13/llvm-project/build/bin/llvm-readelf -r ch6_1.o
+// ~/llvm/13/llvm-project/build/bin/lld -flavor gnu ch6_1.o -o a.out
+// ~/llvm/13/llvm-project/build/bin/llvm-objdump -d a.out
 
 /// start
 int gStart = 3;
