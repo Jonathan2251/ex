17832bf09d7d022aea243ec90c7de7d1b7c409cc
ChungShu <gamma_chen@yahoo.com.tw>
2021-09-22 17:34:59 +0800

    connect to compiler

---
+++ c++/my_post_exam/5/question.cpp
@@ -0,0 +1,53 @@
+Maximum Subarray
+
+Description
+
+Given an array of integers, find a contiguous subarray which has the largest sum.
+
+The subarray should contain at least one number.
+
+Example
+
+Example 1:
+
+Input:
+
+nums = [−2,2,−3,4,−1,2,1,−5,3]
+
+Output:
+
+6
+
+Explanation:
+
+the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+
+Example 2:
+
+Input:
+
+nums = [1,2,3,4]
+
+Output:
+
+10
+
+Explanation:
+
+the contiguous subarray [1,2,3,4] has the largest sum = 10.
+Challenge
+
+Can you do it in time complexity O(n)
+
+
+class Solution {
+public:
+    /**
+     * @param nums: A list of integers
+     * @return: A integer indicate the sum of max subarray
+     */
+    int maxSubArray(vector<int> &nums) {
+        // write your code here
+    }
+};
+
