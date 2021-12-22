11de8df6da51e79bbf5e09b347b7c1c9011af446
Jonathan Chen <gamma_chen@yahoo.com.tw>
2021-12-22 09:16:25 +0000

    remove include

--- clang/vectorize/Godbolt.cpp
+++ clang/vectorize/Godbolt.cpp
@@ -1,15 +1,15 @@
 // turn off slp vectorize
 // https://stackoverflow.com/questions/51461924/how-to-disable-vectorization-in-clang
 
-// ~/llvm/release/build/bin/clang++ -O2 -emit-llvm -S -fno-slp-vectorize Godbolt.cpp -o Godbolt.no-slp-vectorize.ll
-// ~/llvm/release/build/bin/clang++ -O2 -emit-llvm -S -fslp-vectorize Godbolt.cpp -o Godbolt.slp-vectorize.ll
+// work for x86_64 but not for riscv64
+// ~/llvm/release/build/bin/clang++ -O2 -emit-llvm -S -fno-slp-vectorize Godbolt.cpp -o Godbolt.no-slp-vectorize.ll -target x86_64-unknown-linux-gnu
+// ~/llvm/release/build/bin/clang++ -O2 -emit-llvm -S -fslp-vectorize Godbolt.cpp -o Godbolt.slp-vectorize.ll -target x86_64-unknown-linux-gnu
 
-#include <cstdint>
+#define N 10
 
-using std::uint32_t;
+typedef unsigned int uint32_t;
 
-template <uint32_t N>
-int32_t countsearch(const uint32_t *base, uint32_t needle) {
+uint32_t countsearch(const uint32_t *base, uint32_t needle) {
     uint32_t count = 0;
     for (const uint32_t *probe = base; probe < base + N; probe++) {
         if (*probe < needle)
@@ -17,6 +17,3 @@ int32_t countsearch(const uint32_t *base, uint32_t needle) {
     }
     return count;
 }
-
-template
-int32_t countsearch<10>(const uint32_t *base, uint32_t needle);
