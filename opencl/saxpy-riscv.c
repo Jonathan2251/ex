9230091c34147b99e0401686b3ac50760a33f476
Jonathan Chen <gamma_chen@yahoo.com.tw>
2022-08-02 08:10:34 +0000

    saxpy-riscv.c: global(DRAM) local(SRAM)

--- opencl/saxpy-riscv.c
+++ opencl/saxpy-riscv.c
@@ -12,10 +12,12 @@ void saxpy_kernel(float alpha,
                   __global float *C)      
 {
   // global: DRAM, local: SRM
-  //Get the index of the work-item     
-  // The call to get_global_id() returns different values for each work- item referring to different points in the iteration space 
+  //Get the index of the work-item
+  // size_t get_global_id(uint dimension): Returns the index of the current work-item in the global space in the requested dimension
+  // The call to get_global_id() returns different values for each work-item referring to different points in the iteration space 
   // PE0: index=0, PE1: index=1024
   int from = get_global_id(0);
+  // size_t get_local_id(uint dimension): Returns the index of the current work-item as an offset from the beginning of the current work-group
   //int from = get_local_id(0);
   int to = from + 1024;
 #ifdef riscv_scalar
