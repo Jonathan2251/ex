b77e353db49f223acc8985d2cc7b3715757ed5a5
Jonathan Chen <gamma_chen@yahoo.com.tw>
2022-08-02 08:05:26 +0000

    saxpy-riscv.c: global(DRAM) local(SRAM)

--- opencl/saxpy-riscv.c
+++ opencl/saxpy-riscv.c
@@ -3,15 +3,29 @@
 //OpenCL kernel which is run for every work item created.
 #define __global __attribute__((address_space(1)))
 int get_global_id(int index);
+int get_local_id(int index);
 
 //__kernel: mask for LT's DSL
 void saxpy_kernel(float alpha,    
                   __global float *A,      
                   __global float *B,      
                   __global float *C)      
-{                                         
-    //Get the index of the work-item      
-    int index = get_global_id(0);         
-    C[index] = alpha* A[index] + B[index];
+{
+  // global: DRAM, local: SRM
+  //Get the index of the work-item     
+  // The call to get_global_id() returns different values for each work- item referring to different points in the iteration space 
+  // PE0: index=0, PE1: index=1024
+  int from = get_global_id(0);
+  //int from = get_local_id(0);
+  int to = from + 1024;
+#ifdef riscv_scalar
+  for (int i = from; i < to; i++) {   
+    C[i] = alpha* A[i] + B[i];
+  }
+#elif riscv_rvv
+    // riscv vector vmul and vadd
+#elif BMC
+    // BMC vmul
+#endif
 }                                         
 
