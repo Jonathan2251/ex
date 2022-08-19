eb2ed740f7d85be507c4acd7910a0c229bd053e5
Jonathan Chen <gamma_chen@yahoo.com.tw>
2022-08-19 09:38:14 +0000

    pass UVec32

--- opencl/vector-dsl/vector-dsl.cpp
+++ opencl/vector-dsl/vector-dsl.cpp
@@ -25,25 +25,25 @@ UVec32::UVec32(uint32_t *A, size_t aSize) {
 }
 
 UVec32& UVec32::Mul(const uint32_t Scalar) {
-  static UVec32 res(t);
+  static UVec32 res(t, size);
   size_t n = size;
   uint32_t *a = res.data;
   uint32_t *b = data;
+#ifdef USE_RVV
   while (n > 0) {
-  #ifdef USE_RVV
-    size_t vl = vsetvl_e32m8(32);
+    size_t vl = vsetvl_e32m8(n);
     vuint32m8_t vb = vle32_v_u32m8(b, vl);
     vuint32m8_t va = vmul(vb, Scalar, vl);
     vse32(a, va, vl);
-  #else
-    for (int i=0; i < 32; i++) {
-      a[i] = Scalar*b[i];
-    }
-  #endif
     a += vl;
     b += vl;
     n -= vl;
   }
+#else
+  for (int i=0; i < size; i++) {
+    a[i] = Scalar*b[i];
+  }
+#endif
   return res;
 }
 
@@ -52,28 +52,29 @@ UVec32& UVec32::operator*(const uint32_t Scalar) {
 } 
 
 UVec32& UVec32::operator+(const UVec32 &arg) {
-  static UVec32 res(t);
+  static UVec32 res(t, size);
   size_t n = size;
   uint32_t *a = res.data;
   uint32_t *b = data;
   uint32_t *c = arg.data;
+  assert(size == arg.size);
+#ifdef USE_RVV
   while (n > 0) {
-  #ifdef USE_RVV
-    size_t vl = vsetvl_e32m8(32);
+    size_t vl = vsetvl_e32m8(n);
     vuint32m8_t vb = vle32_v_u32m8(b, vl);
     vuint32m8_t vc = vle32_v_u32m8(c, vl);
     vuint32m8_t va = vadd(vb, vc, vl);
     vse32(a, va, vl);
-  #else
-    for (int i=0; i < 32; i++) {
-      a[i] = b[i]+c[i];
-    }
-  #endif
     a += vl;
     b += vl;
     c += vl;
     n -= vl;
   }
+#else
+  for (int i=0; i < size; i++) {
+    a[i] = b[i]+c[i];
+  }
+#endif
   return res;
 }
 
@@ -88,16 +89,23 @@ UVec32& operator*(const uint32_t Scalar, UVec32 &B) {
   return B * Scalar;
 }
 
-uint32_t gV[33*3] = {
+uint32_t gV1[33] = {
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 
+};
+
+uint32_t gV2[33] = {
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+};
+
+uint32_t gV3[33] = {
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
 };
 
 int main() {
-  UVec32 A(gV,33), B(gV+33,33), C(gV+66,33);
+  UVec32 A(gV1,array_size(gV1)), B(gV2,array_size(gV2)), C(gV3,array_size(gV3));
   uint32_t alpha = (uint32_t)2;
-  A = alpha*B + C;
+  //A = alpha*B + C;
+  A = alpha*B;
   printf("B: "); B.Print();
   printf("C: "); C.Print();
   printf("A: "); A.Print();
--- opencl/vector-dsl/vector-dsl.h
+++ opencl/vector-dsl/vector-dsl.h
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stddef.h>
 
 enum Precision {
   Bit32,
