99c346ddaf1404bb23a6c82326d6a8f30bb07b6d
ChungShu <gamma_chen@yahoo.com.tw>
2022-08-19 17:06:03 +0800

    vector-dsl

---
+++ opencl/vector-dsl/vector-dsl.cpp
@@ -0,0 +1,112 @@
+/* ~/riscv/riscv_newlib/bin/clang++ vector-dsl.cpp -menable-experimental-extensions \
+-march=rv64gcv0p10 -O0 -mllvm --riscv-v-vector-bits-min=256 -v */
+// ~/riscv/git/qemu/build/qemu-riscv64 -cpu rv64,v=true a.out
+// ~/riscv/riscv_newlib/bin/riscv64-unknown-elf-objdump -d a.out|grep vmul
+
+#include "vector-dsl.h"
+#include <assert.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <riscv_vector.h>
+
+#define USE_RVV
+
+#define array_size(a) (sizeof(a) / sizeof((a)[0]))
+
+const Precision bit16 = Bit16;
+
+uint32_t t[33] = {
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+};
+
+UVec32::UVec32(uint32_t *A, size_t aSize) {
+  data = A;
+  size = aSize;
+}
+
+UVec32& UVec32::Mul(const uint32_t Scalar) {
+  static UVec32 res(t);
+  size_t n = size;
+  uint32_t *a = res.data;
+  uint32_t *b = data;
+  while (n > 0) {
+  #ifdef USE_RVV
+    size_t vl = vsetvl_e32m8(32);
+    vuint32m8_t vb = vle32_v_u32m8(b, vl);
+    vuint32m8_t va = vmul(vb, Scalar, vl);
+    vse32(a, va, vl);
+  #else
+    for (int i=0; i < 32; i++) {
+      a[i] = Scalar*b[i];
+    }
+  #endif
+    a += vl;
+    b += vl;
+    n -= vl;
+  }
+  return res;
+}
+
+UVec32& UVec32::operator*(const uint32_t Scalar) {
+  return Mul(Scalar);
+} 
+
+UVec32& UVec32::operator+(const UVec32 &arg) {
+  static UVec32 res(t);
+  size_t n = size;
+  uint32_t *a = res.data;
+  uint32_t *b = data;
+  uint32_t *c = arg.data;
+  while (n > 0) {
+  #ifdef USE_RVV
+    size_t vl = vsetvl_e32m8(32);
+    vuint32m8_t vb = vle32_v_u32m8(b, vl);
+    vuint32m8_t vc = vle32_v_u32m8(c, vl);
+    vuint32m8_t va = vadd(vb, vc, vl);
+    vse32(a, va, vl);
+  #else
+    for (int i=0; i < 32; i++) {
+      a[i] = b[i]+c[i];
+    }
+  #endif
+    a += vl;
+    b += vl;
+    c += vl;
+    n -= vl;
+  }
+  return res;
+}
+
+void UVec32::Print() {
+  for (int i=0; i < size; i++) {
+    printf("%u ", data[i]);
+  }
+  printf("\n");
+}
+
+UVec32& operator*(const uint32_t Scalar, UVec32 &B) {
+  return B * Scalar;
+}
+
+uint32_t gV[33*3] = {
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+};
+
+int main() {
+  UVec32 A(gV,33), B(gV+33,33), C(gV+66,33);
+  uint32_t alpha = (uint32_t)2;
+  A = alpha*B + C;
+  printf("B: "); B.Print();
+  printf("C: "); C.Print();
+  printf("A: "); A.Print();
+  printf("\n\n");
+
+  // multiply and add, madd
+  // A = B * C + D; 
+  // Solve above by class is not efficient, class does not use madd.
+  // DSL: extend clang to parsing the left and calling A = madd(B,C,D); can get better performance.
+
+  return 0;
+}
